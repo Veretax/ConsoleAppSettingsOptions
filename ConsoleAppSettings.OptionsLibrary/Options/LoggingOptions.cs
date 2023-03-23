729d120b07926bd4df44dfcea43f8e4202966e26
westerntc <veretax@gmail.com>
2023-03-23 19:40:49 -0400

    Cleaned up last GetSection References

--- ConsoleAppSettings.OptionsLibrary/Options/LoggingOptions.cs
+++ ConsoleAppSettings.OptionsLibrary/Options/LoggingOptions.cs
@@ -9,32 +9,6 @@ public sealed class LoggingOptions : AbstractConfigurationOptions, IConfiguratio
     public const string LoggingName = "Logging";
     public LogLevelOptions? LogLevel { get; set; } = new LogLevelOptions();
     
-    //public IConfiguration GetSection(IConfiguration config)
-    //{
-    //    LoggingOptions options = new();
-
-    //    var section = config.GetSection(LoggingOptions.LoggingName);
-    //    if (section.Exists())
-    //    {
-    //        section.Bind(this);
-    //    }
-    //    else
-    //    {
-            
-    //        options.LogLevel = new LogLevelOptions()
-    //        {
-    //            Default = DefaultApplicationOptions.DefaultLoggingLevel,
-    //            MicrosoftAspNetCore = DefaultApplicationOptions.DefaultMicrosoftAspNetCoreLoggingLevel
-    //        };
-
-    //        this.LogLevel = options.LogLevel;
-    //        section.Bind(options);
-    //    }
-        
-    //    return config;
-    //}
-
-
     public LoggingOptions BindOptions(LoggingOptions options)
     {
         if (Configuration == null)
